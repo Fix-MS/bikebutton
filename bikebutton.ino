f3b81afafa8eb44ff28e247541a372e964e4ad13
anticultist <33195947+anticultist@users.noreply.github.com>
2022-09-23 21:47:42 +0200

    add wifi connection code

--- bikebutton.ino
+++ bikebutton.ino
@@ -1,5 +1,12 @@
 #include <ezButton.h>
+#include <WiFi.h>
 
+// NOTE: please create a local file with the following variables
+// const char* ssid = "...";
+// const char* password = "...";
+#include "wifi_credentials.h"
+
+//// Button
 #define VALIDATION_TIME 3000
 
 ezButton button(23); // create ezButton object that attach to pin GIOP21
@@ -13,6 +20,19 @@ bool sendEvent = true;
 void setup() {
   Serial.begin(9600);
   button.setDebounceTime(50); // set debounce time to 50 milliseconds
+
+  //Initiate WiFi connection
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  // Wait for connection
+  Serial.print("\ntrying to connect to Wifi!\n");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("WiFi connected with IP: ");
+  Serial.println(WiFi.localIP());
 }
 
 void send() {
@@ -21,6 +41,7 @@ void send() {
   sendEvent = true;
   numberOfTimes = 0;
 
+  // TODO: send request to our server
 }
 
 void loop() {
